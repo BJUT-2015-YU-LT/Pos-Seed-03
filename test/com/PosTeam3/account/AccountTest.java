b4627cb9c531fcc016350159d033ca4bd3c3116a
hddxdx <hddxds@outlook.com>
2016-01-16 15:22:53 +0800

    总结
    总结

---
+++ test/com/PosTeam3/account/AccountTest.java
@@ -0,0 +1,31 @@
+package com.PosTeam3.account;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 硕 on 2016/1/16.
+ */
+public class AccountTest {
+
+    @Test
+    public void testScan() throws Exception {
+
+    }
+
+    @Test
+    public void testAccount() throws Exception {
+
+    }
+
+    @Test
+    public void testAccountSaved() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUserInfo() throws Exception {
+
+    }
+}
